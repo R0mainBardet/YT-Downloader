7ce0cc6e4ee86c299d1c5e185bee00f21b04066d
R0mainBardet <romain.bardet@live.fr>
2025-02-25 20:06:18 +0100

    Change of Module and playlist integration

--- downloader.py
+++ downloader.py
@@ -1,10 +1,26 @@
-import pytube import Youtube
+import os
+import yt_dlp
 
-url = input("Url Youtube")
-yt = Youtube(url)
+def download_youtube_playlist(url, output_folder):
+    os.makedirs(output_folder, exist_ok=True)
+    
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+        'outtmpl': os.path.join(output_folder, '%(playlist)s/%(title)s.%(ext)s'), 
+        'yesplaylist': True
+    }
+    
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+    
+    print("Téléchargement de la playlist terminé !")
 
-stream = yt.streams.get_audio_only()
-
-stream.download()
-print("Réussi")
-
+if __name__ == "__main__":
+    youtube_url = input("Entrez l'URL de la playlist YouTube Music : ")
+    output_folder = input("Entrez le dossier de destination : ")
+    download_youtube_playlist(youtube_url, output_folder)
